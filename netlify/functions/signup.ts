4817bb70c4c11f003f8e889ea01acc88633960ff
Gyen Abubakar <indehyde@gmail.com>
2022-02-04 10:16:51 +0000

    node-fetch for making requests

--- netlify/functions/signup.ts
+++ netlify/functions/signup.ts
@@ -1,18 +1,26 @@
 import { Handler } from '@netlify/functions';
+import fetch from 'node-fetch';
 import GoTrueAuth from '../lib/gotrue';
 import { SignupInfo } from '../lib/types';
+
+// @ts-ignore
+global.fetch = fetch;
 
 export const handler: Handler = async (event, context) => {
 	if (event.body) {
 		const { email, password, data } = JSON.parse(event.body) as SignupInfo;
 
 		try {
-			const user = await GoTrueAuth.signup(email, password);
+			const user = await GoTrueAuth.signup(email, password, data);
+
 			return {
 				statusCode: 200,
-				user,
+				data,
+				body: JSON.stringify(user),
 			};
 		} catch (error) {
+			console.log(error);
+
 			return {
 				statusCode: 500,
 				error,
